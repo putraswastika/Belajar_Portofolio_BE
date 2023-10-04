f50d634ed268aa623bc141424de36e4f6db770a3
putra_swastika <iketutputraswastika@gmail.com>
2023-10-04 12:31:46 +0800

    fourth-commit

--- server.js
+++ server.js
@@ -18,33 +18,47 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+const sentEmails = [];
+
 // Route handling
 app.get('/', (req, res) => {
   res.send('Selamat datang di API Belajar Portofolio BE.');
 });
 
 app.post('/api/email', (req, res) => {
-  const { name, from, subject, message } = req.body;
-  const emailInfo = {
-    to: 'putraswastikay@gmail.com',
-    name: name,
-    from: from,
-    subject: subject,
-    html: message
-  };
-
-  mailgun.messages().send(emailInfo, (error, body) => {
-    if (error) {
-      console.error(error);
-      res.status(500).send({
-        message: 'Ada kesalahan dalam mengirim email!'
-      });
-    } else {
-      res.send({ message: 'Email berhasil dikirim!' });
-    }
+    const { name, from, subject, message } = req.body;
+    const emailInfo = {
+      to: 'putraswastikay@gmail.com',
+      name: name,
+      from: from,
+      subject: subject,
+      html: message
+    };
+  
+    mailgun.messages().send(emailInfo, (error, body) => {
+      if (error) {
+        console.error(error);
+        res.status(500).send({
+          message: 'Ada kesalahan dalam mengirim email!'
+        });
+      } else {
+        const sentEmail = {
+          name: name,
+          from: from,
+          subject: subject,
+          message: message
+        };
+        sentEmails.push(sentEmail);
+        res.send({ message: 'Email berhasil dikirim!' });
+      }
+    });
   });
-});
-
-app.listen(PORT, () => {
-  console.log(`Server berjalan di port ${PORT}`);
-});
+  
+  // Route untuk melihat data email yang telah dikirim
+  app.get('/api/email', (req, res) => {
+    res.json(sentEmails);
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Server berjalan di port ${PORT}`);
+  });   
