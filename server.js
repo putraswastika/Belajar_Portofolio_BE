872a2dd4b485c9914946972a00170389225c84ee
putra_swastika <iketutputraswastika@gmail.com>
2023-10-04 11:59:57 +0800

    fourth-commit

--- server.js
+++ server.js
@@ -1,29 +1,29 @@
-const express = require("express");
-const app = express();
-const dotenv = require("dotenv");
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
 const mg = require("mailgun-js")
-const cors = require("cors")
 
+const routes = require('./routes');
 
+const app = express();
 const PORT = process.env.PORT || 5000;
-
-dotenv.config()
 
 const mailgun = () => mg ({
     apiKey: process.env.MAILGUN_API_KEY,
     domain: process.env.MAILGUN_DOMAIN
 })
 
+const router = express.Router();
 
-//middleware
-app.use(express.static('./'))
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use(cors())
 
-/* app.get('/',(req,res)=>{
-res.sendFile(__dirname + './')
-}) */
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use((req, res, next) => {
+    console.log(`Method: ${req.method} ${req.path}`);
+    next();
+});
 
 app.post('/api/email',(req,res)=>{
     const {name,from,subject,message}=req.body,
@@ -47,6 +47,12 @@ app.post('/api/email',(req,res)=>{
     })
 })
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`)
-})
+
+app.use(routes.bookingRoutes.router);
+
+// routes.forEach((route) => app.use(route));
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
