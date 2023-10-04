d2b88e61ca43df51f3abb21cf19f0e0eaf951446
putra_swastika <iketutputraswastika@gmail.com>
2023-10-04 12:06:58 +0800

    fourth-commit

--- server.js
+++ server.js
@@ -1,58 +1,50 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const mg = require("mailgun-js")
-
-const routes = require('./routes');
-
+const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mg = require("mailgun-js");
+const cors = require("cors");
+
+dotenv.config();
+
 const PORT = process.env.PORT || 5000;
 
-const mailgun = () => mg ({
-    apiKey: process.env.MAILGUN_API_KEY,
-    domain: process.env.MAILGUN_DOMAIN
-})
-
-const router = express.Router();
-
-
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
-app.use((req, res, next) => {
-    console.log(`Method: ${req.method} ${req.path}`);
-    next();
+const mailgun = mg({
+  apiKey: process.env.MAILGUN_API_KEY,
+  domain: process.env.MAILGUN_DOMAIN
 });
 
-app.post('/api/email',(req,res)=>{
-    const {name,from,subject,message}=req.body,
-    emailInfo = {
-        to: '<putraswastikay@gmail.com>',
-        name: `${name}`,
-        from: `${from}`,
-        subject: `${subject}`,
-        html: `${message}`
+// Middleware
+app.use(express.static('./'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html'); // Sesuaikan dengan nama file HTML Anda
+});
+
+app.post('/api/email', (req, res) => {
+  const { name, from, subject, message } = req.body;
+  const emailInfo = {
+    to: 'putraswastikay@gmail.com',
+    name: name,
+    from: from,
+    subject: subject,
+    html: message
+  };
+
+  mailgun.messages().send(emailInfo, (error, body) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send({
+        message: 'Something went wrong in sending email!'
+      });
+    } else {
+      res.send({ message: 'Email sent successfully!' });
     }
-
-    mailgun().messages().send(emailInfo,(error,body) =>{
-        if (error){
-            console.log(error)
-            res.status(500).send({
-                message: 'Something went wrong in sending email!'
-            })
-        }else{
-            res.send({message:'Email sent successfully!'})
-        }
-    })
-})
-
-
-app.use(routes.bookingRoutes.router);
-
-// routes.forEach((route) => app.use(route));
-
+  });
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
